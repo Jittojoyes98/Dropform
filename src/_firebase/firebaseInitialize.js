e43bca1b67d768e346f23b204bc2244e09f172d4
Jittojoyes98 <51293782+Jittojoyes98@users.noreply.github.com>
2023-04-06 20:59:09 +0530

    Setting firebase store

--- src/_firebase/firebaseInitialize.js
+++ src/_firebase/firebaseInitialize.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider, setPersistence, browserSessionPersistence } from "firebase/auth"
 import { firebaseMeasurementId,firebaseAppId,firebaseMessagingSenderId,firebaseStorageBucket,firebaseProjectId, firebaseAuthDomain,firebaseApiKey } from "../_helpers/constants"
+import { getFirestore } from "firebase/firestore"
 
 export const firebaseConfig={
     apiKey: firebaseApiKey,
@@ -17,6 +18,7 @@ const app = initializeApp(firebaseConfig)
 // const analytics = getAnalytics(app); // check if this is required or not
 
 export const auth=getAuth(app)
+export const db=getFirestore(app)
 export const persistedAuth = setPersistence(auth, browserSessionPersistence)
 export const provider = new GoogleAuthProvider()
 export default app;
