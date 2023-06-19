ebe2263774b9bd0df12749e67f5fc118ba4df439
Jittojoyes98 <jittojoyes77@gmail.com>
2023-06-19 17:28:57 +0530

    Add : Question and answer part of Text field

--- src/_ui/InputQuestion/InputQuestion.jsx
+++ src/_ui/InputQuestion/InputQuestion.jsx
@@ -1,27 +1,66 @@
 import React from "react";
-import Box from "@mui/material/Box"
-import  Container  from "@mui/material/Container";
-import  Typography  from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import deleteSVG from "../../../assets/delete.svg";
+
+const ariaLabel = { "aria-label": "description" };
 
 const InputQuestion = () => {
   return (
-  <Box className="input-text-wrapper">
-    <Box className="input-text-question">
-        <Container>
-          <Typography>1</Typography>
+    <Box className="input-text-wrapper">
+      <Box className="input-text-question">
+        <Container className="input-text-question-num">
+          <Typography>{`1 ->`}</Typography>
         </Container>
-        <Container>
-
-
+        <Container className="input-text-question-field-wr">
+          <TextField
+            placeholder="Type your question here..."
+            id="filled-hidden-label-small"
+            variant="filled"
+            className="input-text-question-field"
+            InputProps={{
+              disableUnderline: true,
+            }}
+          />
         </Container>
+        {/* reusable part */}
         <Container className="input-text-handle-content">
-          <Typography>Text 1</Typography>
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={2}
+          >
+            <Box className="input-text-handle-content-name">
+              <Typography>Text 1</Typography>
+            </Box>
+            <Box>
+              <img
+                src={deleteSVG}
+                alt="delete"
+                className="input-text-handle-delete"
+              />
+            </Box>
+          </Stack>
         </Container>
+      </Box>
+      <Box>desc</Box>
+      <Box className="">
+        <Container className="input-text-answer-field-wr">
+          <Input
+            className="input-text-answer-field"
+            placeholder="Type your answer here..."
+            inputProps={ariaLabel}
+            disabled={true}
+          />
+        </Container>
+      </Box>
     </Box>
-    
-    
-  </Box>
-  )
+  );
 };
 
 export default InputQuestion;
