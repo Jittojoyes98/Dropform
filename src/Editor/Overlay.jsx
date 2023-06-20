cc51c85feaf09ff135b460c5599771fc271e6b2b
Jittojoyes98 <jittojoyes77@gmail.com>
2023-06-20 17:23:50 +0530

    Fix :  Overlay changes

--- src/Editor/Overlay.jsx
+++ src/Editor/Overlay.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { forwardRef } from "react";
 import { useDndStore } from "./EditorStore";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
 
 const CoreOverlay = forwardRef(({ children, ...props }, ref) => {
   const activeId = useDndStore((state) => state.activeId);
 
   return (
-    <div
-      style={{
-        backgroundColor: "red",
-        width: "fit-content",
-      }}
-      ref={ref}
-      {...props}
-    >
-      {activeId}
-    </div>
+    <Box className="overlay-wrapper" ref={ref} {...props}>
+      <Container>
+        <Typography>{activeId}</Typography>
+      </Container>
+    </Box>
   );
 });
 
