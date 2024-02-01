6fc8e3a920d258e8afb4f3cd91a1d3a3dabef2fb
Jitto Joyes <jittojoyes77@gmail.com>
2024-02-01 15:18:43 +0530

    changes:
    -- Route and pages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,20 @@
-import React, { Suspense } from 'react'
-import { Layout } from '../_layout'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LoginPage from '../LoginPage'
-import HomePage from '../HomePage'
-import SignupPage from '../SignupPage'
-import { Authorize } from '../auth'
-import { Dashboard } from '../Dashboard'
-import { ForgotPassword } from '../ForgotPassword'
-import { PrivateRoute } from '../_components/PrivateRoute'
-import SignUpEmail from '../SignupPage/SignUpEmail'
-import NewPassword from '../ForgotPassword/NewPassword'
-import Editor from '../Editor/Editor'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-import '@/_styles/theme.scss'
+import React, { Suspense } from "react";
+import { Layout } from "../_layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "../LoginPage";
+import HomePage from "../HomePage";
+import SignupPage from "../SignupPage";
+import { Authorize } from "../auth";
+import { Dashboard } from "../Dashboard";
+import { ForgotPassword } from "../ForgotPassword";
+import { PrivateRoute } from "../_components/PrivateRoute";
+import SignUpEmail from "../SignupPage/SignUpEmail";
+import NewPassword from "../ForgotPassword/NewPassword";
+import Editor from "../Editor/Editor";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/_styles/theme.scss";
+import Result from "../Results/Result";
 
 const App = () => {
   return (
@@ -30,11 +31,14 @@ const App = () => {
               <Route path="/dashboard" element={<Layout layout="dashboard" />}>
                 <Route index element={<Dashboard />} />
               </Route>
-              <Route
-                path="/editor/:formid"
-                element={<Layout layout="editor" />}
-              >
+              <Route path="/:formid/edit" element={<Layout layout="editor" />}>
                 <Route index element={<Editor />} />
+              </Route>
+              <Route
+                path="/:formid/results"
+                element={<Layout layout="results" />}
+              >
+                <Route index element={<Result />} />
               </Route>
             </Route>
 
@@ -54,7 +58,7 @@ const App = () => {
         </Authorize>
       </Suspense>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Editor/SortableItems.jsx
+++ src/Editor/SortableItems.jsx
@@ -1,6 +1,10 @@
+import React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+import dropDownSvg from "../../assets/dropdown.svg";
 import { useSortable, defaultAnimateLayoutChanges } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import React from "react";
 import { editorStore } from "./EditorStore";
 
 // function animateLayoutChanges(args) {
@@ -31,15 +35,40 @@ const SortableItems = ({ id, selectedItem, item }) => {
     minHeight: "56px",
   };
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleDelete = () => {
+    setAnchorEl(null);
+    deleteForms(formData.id);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [isHovered, setIsHovered] = React.useState(false);
+
   const openPropertiesClicking = editorStore(
     (state) => state.openPropertiesClicking
   );
+  const handleHoverOver = React.useCallback(() => {
+    setIsHovered(true);
+  }, []);
+  const handleHoverLeave = React.useCallback(() => {
+    setIsHovered(false);
+    handleClose();
+  }, []);
 
   // console.log("HERE", item?.order_id, "====", selectedItem);
 
   return (
     <div
       onClick={() => openPropertiesClicking(item.order_id)}
+      onMouseOver={handleHoverOver}
+      onMouseLeave={handleHoverLeave}
       className={
         selectedItem && item?.order_id == selectedItem
           ? "sortable change"
@@ -51,6 +80,38 @@ const SortableItems = ({ id, selectedItem, item }) => {
       {...listeners}
     >
       <p>{item.question_name}</p>
+      {
+        <>
+          <Box
+            className="form-details-dropdown"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <span>
+              <img
+                src={dropDownSvg}
+                alt="select"
+                className="rotated-dropdown"
+              />
+            </span>
+          </Box>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
+        </>
+      }
     </div>
   );
 };
