e99288d340919e43d4816ebd0bb839ff8c71a4e7
Jitto Joyes <jittojoyes77@gmail.com>
2024-06-24 15:12:21 +0530

    fix: changes to image url.

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { SvgAssets } from "../_helpers/images";
 
 const HomePage = () => {
   return (
     <div className="home-wrapper">
       <div className="home-main">
         <section data-testid="homepage-hero" className="home-hero-section">
-          <img src={`./hero-poster.webp`} alt="hero image" />
+          <img src={"../../public/hero-poster.webp"} alt="hero image" />
           <div className="centre-div-vertical">
             <div>
               <h1 className="big-header">Welcome to dropform</h1>
